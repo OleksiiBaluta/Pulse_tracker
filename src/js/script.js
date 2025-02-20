b4d192193c50b0552dfd9903a7b3c4fa3390524b
Olexii <alexbaluta85@gmail.com>
2025-02-20 17:08:32 +0200

    adaptation logo

--- src/js/script.js
+++ src/js/script.js
@@ -1,7 +1,8 @@
 $(document).ready(function () {
     $('.carousel__inner').slick({
         speed: 1200,
-        // adaptiveHeight: true,
+        fade: true,
+        adaptiveHeight: true,
         prevArrow: '<button type="button" class="slick-prev"><img src="icons/arrow/chevron-left-solid.png" /></button>',
         nextArrow: '<button type="button" class="slick-next"><img src="icons/arrow/chevron-right-solid.png" /></button>',
         responsive: [
